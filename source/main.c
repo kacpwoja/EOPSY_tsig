afd1e4cca48080c78e16e47f1a7d19c8ee852353
Kacper Wojakowski <kacp.woja@gmail.com>
2019-04-09 21:32:37 +0200

    fixed interrupt

--- source/main.c
+++ source/main.c
@@ -20,7 +20,7 @@ int main(int argc, char** argv)
 {
 	#ifdef WITH_SIGNALS
 	// Ignore all signals
-	for(int i=1; i<32; i++)
+	for(int i=1; i<NSIG; i++)
 		signal(i, SIG_IGN);
 	// Restore SIGCHLD
 	signal(SIGCHLD, SIG_DFL);
@@ -28,10 +28,21 @@ int main(int argc, char** argv)
 	signal(SIGINT, interrupt_handler);
 	#endif
 	pid_t child_pids[NUM_CHILD];
+	int children_count = 0;
 	pid_t pid;
 
 	for(int i=0; i<NUM_CHILD; i++)
 	{
+		#ifdef WITH_SIGNALS
+		// Check interrupt
+		if(interrupted != 0)
+		{
+			// If process interrupted, print message and exit loop
+			terminate_children(i, child_pids);
+			printf("parent[%d]: Child creation interrupted.\n", (int)getpid());
+			break;
+		}
+		#endif
 		// Create child process
 		pid = fork();
 
@@ -47,18 +58,10 @@ int main(int argc, char** argv)
 		// In Parent Process
 		if(pid > 0)
 		{
-			#ifdef WITH_SIGNALS
-			if(interrupted != 0)
-			{
-				// If process interrupted, print message and exit loop
-				terminate_children(i, child_pids);
-				printf("parent[%d]: Child creation interrupted.\n", (int)getpid());
-				break;
-			}
-			#endif
 			// Add to PID array
 			child_pids[i] = pid;
 			printf("parent[%d]: Child process created. PID: %d\n", (int)getpid(), (int)child_pids[i]);
+			++children_count;
 
 			// Wait for 1 second
 			sleep(1);
@@ -78,16 +81,16 @@ int main(int argc, char** argv)
 			return 0;
 		}
 	}
-	int exit_codes[NUM_CHILD];	
+	int exit_codes[children_count];	
 	int finished = 0;
 	int code;
-	while(finished < NUM_CHILD)
+	while(finished < children_count)
 	{
 		// Wait for child to exit
 		pid = wait(&code);
 
 		// Save exit code
-		for(int i=0; i<NUM_CHILD; i++)
+		for(int i=0; i<children_count; i++)
 		{
 			if(child_pids[i] == pid)
 			{
@@ -100,15 +103,15 @@ int main(int argc, char** argv)
 	}
 
 	// Print ending message and all exit codes
-	printf("parent[%d]: No more children remaining.List of all exit codes:\n", (int)getpid());
-	for(int i=0; i<NUM_CHILD; i++)
+	printf("parent[%d]: No more children remaining. List of all exit codes:\n", (int)getpid());
+	for(int i=0; i<children_count; i++)
 	{
 		printf("PID: %d Exit Code: %d\n", child_pids[i], exit_codes[i]);
 	}
 
 	#ifdef WITH_SIGNALS
 	// Return signal handlers
-	for(int i=1; i<32; i++)
+	for(int i=1; i<NSIG; i++)
 		signal(i, SIG_DFL);
 	#endif
 	return 0;
